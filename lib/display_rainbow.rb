df041e5c8d3b11185cc630ed8a3cd4802df820b3
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-29 18:58:18 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow (colors)
-  puts "#{colors[0].initials} : #{colors[0]},#{colors[1].initials} : #{colors[1]},#{colors[2].initials} : #{colors[2]}, #{colors[3].initials} : #{colors[3]} "
+  puts "#{colors[0].initials} : #{colors[0]}"
 end 
